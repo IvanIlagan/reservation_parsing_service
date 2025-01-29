0dfc7faba3d7decdc888aabd588293bf195a7d19
Genesis Ivan Ilagan <ivanilagan1109@gmail.com>
2025-01-30 00:38:25 +0800

    (enhance) Added Additional Test Scenarios on Request Test

--- spec/requests/reservations_request_spec.rb
+++ spec/requests/reservations_request_spec.rb
@@ -77,6 +77,38 @@ RSpec.describe "Reservation Request", type: :request do
         expect { endpoint }.to change(Guest, :count).from(1).to(2)
       end
 
+      context "when some of the keys was not provided in the payload" do
+        let!(:payload) do
+          {
+            "reservation": {
+              "start_date": nil,
+              "end_date": "",
+              "expected_payout_amount": "3800.00",
+              "guest_details": {
+                "localized_description": "4 guests",
+                "number_of_adults": 2,
+                "number_of_children": 2,
+                "number_of_infants": 0
+              },
+              "guest_email": "wayne_woodbridge@bnb.com",
+              "guest_first_name": "Wayne",
+              "guest_id": 1,
+              "guest_last_name": "Woodbridge",
+              "guest_phone_numbers": [
+                "639123456789",
+                "639123456781"
+              ],
+              "listing_security_price_accurate": "500.00",
+              "total_paid_amount_accurate": "4500.00"
+            }
+          }
+        end
+
+        it "does not create a Reservation data", :requires_new_request do
+          expect { endpoint }.not_to change(Reservation, :count)
+        end
+      end
+
       context "when given guest in payload already exists in the system" do
         let(:guest) { create(:guest, email: "new@email.com", first_name: "New", last_name: "1") } 
         let!(:payload) do
@@ -155,6 +187,36 @@ RSpec.describe "Reservation Request", type: :request do
 
       it "successfully creates a new guest", :requires_new_request do
         expect { endpoint }.to change(Guest, :count).from(1).to(2)
+      end
+
+      context "when some of the keys was not provided in the payload" do
+        let!(:payload) do
+          {
+            "start_date": "",
+            "end_date": "2023-07-13",
+            "nights": nil,
+            "guests": nil,
+            "adults": nil,
+            "children": 0,
+            "infants": 0,
+            "status": "accepted",
+            "guest": {
+              "id": 1,
+              "first_name": "Wayne",
+              "last_name": "Woodbridge",
+              "phone": "639123456789",
+              "email": "wayne_woodbridge@bnb.com"
+            },
+            "currency": "AUD",
+            "payout_price": "1800.00",
+            "security_price": "500",
+            "total_price": "2500.00"
+          }
+        end
+
+        it "does not create a Reservation data", :requires_new_request do
+          expect { endpoint }.not_to change(Reservation, :count)
+        end
       end
 
       context "when given guest in payload already exists in the system" do
